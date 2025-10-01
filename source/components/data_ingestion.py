581a24612e2769579f92d4423fc442155a78004a
ganesh <tarigondaganesh1234@gmail.com>
2025-10-01 23:19:08 +0530

    modeltrainer

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -6,6 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+from source.components.model_trainer import ModelTrainer
+from source.components.model_trainer import ModelTrainerConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -39,4 +42,6 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,preprocessor_path=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr,preprocessor_path))
--- source/components/model_trainer.py
+++ source/components/model_trainer.py
@@ -3,11 +3,16 @@ import os
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import r2_score
 from source.exception import customException
 from source.logger import logging
-from source.utils import save_object
+from catboost import CatBoostRegressor
+from source.utils import evaluate_models, save_object
 
 @dataclass
 class ModelTrainerConfig:
@@ -17,7 +22,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array):
+    def initiate_model_trainer(self, train_array, test_array,preprocessor_path):
         try:
             logging.info("Splitting training and test input data")
             X_train, y_train, X_test, y_test = (
@@ -27,21 +32,35 @@ class ModelTrainer:
                 test_array[:, -1]
             )
 
-            model = LinearRegression()
-            model.fit(X_train, y_train)
-            logging.info("Model training completed")
-
-            y_pred = model.predict(X_test)
-            r2 = r2_score(y_test, y_pred)
-            logging.info(f"R2 score on test data: {r2}")
-
+            models={
+                  "Linear Regression": LinearRegression(),
+    "Lasso": Lasso(),
+    "Ridge": Ridge(),
+    "K-Neighbors Regressor": KNeighborsRegressor(),
+    "Decision Tree": DecisionTreeRegressor(),
+    "Random Forest Regressor": RandomForestRegressor(),
+    "XGBRegressor": XGBRegressor(),
+    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+    "AdaBoost Regressor": AdaBoostRegressor()
+            }
+            model_report:dict=evaluate_models(x_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,models=models)
+            best_model_score=max(sorted(model_report.values()))
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model=models[best_model_name]
+            if best_model_score<0.6:
+                raise customException("no best model found")
+            logging.info("best model found for training and testing")
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=model
+                obj=best_model
             )
-            logging.info("Model saved successfully")
-
-            return r2
-
+            predicted=best_model.predict(X_test)
+            r2_square=r2_score(y_test,predicted)
+            return r2_square
         except Exception as e:
-            raise customException(e, sys)
+            raise customException(e,sys)
+            
+        
+            
