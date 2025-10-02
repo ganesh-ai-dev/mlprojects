e3491e5587d63e2461bfac0cd6a6e16b8ab5dab4
ganesh <tarigondaganesh1234@gmail.com>
2025-10-02 18:42:57 +0530

    deployment

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,request,render_template
 import pandas as pd
 import numpy as np
@@ -29,7 +30,8 @@ def predict_datapoint():
         return render_template("home.html",results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
         
  
